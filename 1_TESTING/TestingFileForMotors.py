b20d498f6bd14e13a8bfa6e030a43eec350d0355
bigbrotherslorenzo <168143754+bigbrotherslorenzo@users.noreply.github.com>
2024-05-02 14:08:34 -0230

    Thing

--- 1_TESTING/TestingFileForMotors.py
+++ 1_TESTING/TestingFileForMotors.py
@@ -15,14 +15,14 @@ dcMotor0.setChannel(0)
 dcMotor1.setDeviceSerialNumber(487701)
 dcMotor1.setChannel(1)
 
-dcMotor2.setDeviceSerialNumber(487701)
+dcMotor2.setDeviceSerialNumber(487901)
 dcMotor2.setChannel(0)
-dcMotor3.setDeviceSerialNumber(487701)
+dcMotor3.setDeviceSerialNumber(487901)
 dcMotor3.setChannel(1)
 
-dcMotor4.setDeviceSerialNumber(487701)
+dcMotor4.setDeviceSerialNumber(487794)
 dcMotor4.setChannel(0)
-dcMotor5.setDeviceSerialNumber(487701)
+dcMotor5.setDeviceSerialNumber(487794)
 dcMotor5.setChannel(1)
 #-----------------------------------
 currentInput0 = CurrentInput()
@@ -48,40 +48,46 @@ def motor_one(throttle):
 	currentInput0.openWaitForAttachment(1000)
 	currentInput0.setOnCurrentChangeHandler(onCurrentChange)
 
-def motor_two():
+def motor_two(throttle):
+	if throttle == None:
+		throttle = 1
 	dcMotor1.openWaitForAttachment(1000)
 	dcMotor1.setAcceleration(19.4)
-	dcMotor1.setTargetVelocity(1)
+	dcMotor1.setTargetVelocity(throttle)
 
 	currentInput1.openWaitForAttachment(1000)
 	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
-def motor_three():
+def motor_three(throttle):
+	if throttle == None:
+		throttle = 1
 	dcMotor2.openWaitForAttachment(1000)
 	dcMotor2.setAcceleration(19.4)
-	dcMotor2.setTargetVelocity(1)
+	dcMotor2.setTargetVelocity(throttle)
 
-	currentInput1.openWaitForAttachment(1000)
 	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
-def motor_four():
+def motor_four(throttle):
+	if throttle == None:
+		throttle = 1
 	dcMotor3.openWaitForAttachment(1000)
 	dcMotor3.setAcceleration(19.4)
-	dcMotor3.setTargetVelocity(1)
-
-	currentInput1.openWaitForAttachment(1000)
+	dcMotor3.setTargetVelocity(throttle)
 	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
-def motor_five():
+def motor_five(throttle):
+	if throttle == None:
+		throttle = 1
 	dcMotor4.openWaitForAttachment(1000)
 	dcMotor4.setAcceleration(19.4)
-	dcMotor4.setTargetVelocity(1)
+	dcMotor4.setTargetVelocity(throttle)
 
-	currentInput1.openWaitForAttachment(1000)
 	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
-def motor_six():
+def motor_six(throttle):
+	if throttle == None:
+		throttle = 1
 	dcMotor5.openWaitForAttachment(1000)
 	dcMotor5.setAcceleration(19.4)
-	dcMotor5.setTargetVelocity(1)
+	dcMotor5.setTargetVelocity(throttle)
 
-	currentInput1.openWaitForAttachment(1000)
+	
 	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
 
 def motor_one_close():
@@ -104,9 +110,14 @@ def motor_six_close():
 
 if __name__ == "__main__":
 	motor_one(1)
-	motor_two()
-	motor_three()
-	motor_four()
-	motor_five()
-	motor_six()
+	time.sleep(5)
+	motor_two(1)
+	time.sleep(5)
+	motor_three(1)
+	time.sleep(5)
+	motor_four(1)
+	time.sleep(5)
+	motor_five(1)
+	time.sleep(5)
+	motor_six(1)
 	time.sleep(5)
